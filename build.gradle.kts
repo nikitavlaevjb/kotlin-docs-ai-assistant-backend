faac6bbf8cb0d7999f5d248a66a19ade36bfa07a
zoobestik <kb.chernenko@gmail.com>
2025-09-17 02:30:18 +0200

    Add Spring Security dependency and implement basic security configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("ai.grazie.api:api-gateway-client-jvm:0.8.54")
